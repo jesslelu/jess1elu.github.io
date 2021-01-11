e0b4e1e38fca8b056e4dae6f8330b1c1eda38435
jessie1231 <44041360+jessie1231@users.noreply.github.com>
2021-01-11 16:50:09 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+anime({
+    
+    targets:'div.header',
+    translateX:[
+        {value:2400, duration:45000},],
+
+    rotate:[{
+        value: '1.6turn',duration:45000},],
+        loop:false,
+
+}
+);
+
+anime({
+    
+    targets:'div.outer',
+    translateX:[
+        {value:1400, duration:45000},]},
+
+
+    
+
+);
