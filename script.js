4df9d64493eb2c88f2b508cd02c7cf9db8137e42
jessie1231 <44041360+jessie1231@users.noreply.github.com>
2021-01-12 12:43:14 -0500

    Changed projects format, added smooth scroll

--- script.js
+++ script.js
@@ -23,23 +23,11 @@ anime({
 );
 
 
-
-//Get the button:
-var mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+function scrollToTop() {
+  var position =
+      document.body.scrollTop || document.documentElement.scrollTop;
+  if (position) {
+      window.scrollBy(0, -Math.max(1, Math.floor(position / 10)));
+      scrollAnimation = setTimeout("scrollToTop()", 30);
+  } else clearTimeout(scrollAnimation);
 }
