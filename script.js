c5642cc0618a41cccab5348c39cd1228af3f20f5
jessie1231 <44041360+jessie1231@users.noreply.github.com>
2021-01-12 13:15:47 -0500

    images

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-anime({
-    
-    targets:'div.header',
-    translateX:[
-        {value:2400, duration:45000},],
-
-    rotate:[{
-        value: '1.6turn',duration:45000},],
-        loop:false,
-
-}
-);
-
-anime({
-    
-    targets:'div.outer',
-    translateX:[
-        {value:1400, duration:45000},]},
-
-
-    
-
-);
 
 
 function scrollToTop() {
